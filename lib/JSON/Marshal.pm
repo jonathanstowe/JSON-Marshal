8ba390899e859f12c54993fbe350fb868395f51f
Jonathan Stowe <jns+git@gellyfish.co.uk>
2015-10-30 22:06:53 +0000

    Add pod

--- lib/JSON/Marshal.pm
+++ lib/JSON/Marshal.pm
@@ -1,6 +1,45 @@
 use v6;
 
-module JSON::Marshal {
+=begin pod
+
+=head1 NAME
+
+JSON::Marshal - Make JSON from an Object.
+
+=head1 SYNOPSIS
+
+=begin code
+    use JSON::Marshal;
+
+    class SomeClass {
+      has Str $.string;
+      has Int $.int;
+    }
+
+    my $object = SomeClass.new(string => "string", int => 42);
+
+
+    my Str $json = marshal($object); # -> "{ "string" : "string", "int" : 42 }'
+
+=end code
+
+=head1 DESCRIPTION
+
+This provides a single exported subroutine to create a JSON representation
+of an object.  It should round trip back into an object of the same class
+using L<JSON::Unmarshal|https://github.com/tadzik/JSON-Unmarshal>.
+
+It only outputs the "public" attributes (that is those with accessors
+created by declaring them with the '.' twigil. Attributes without acccessors
+are ignored.
+
+This is intended to work with objects of arbitrary classes without changes,
+though clearly some loss of fidelity may occur as JSON may not carry all
+the information that a Perl 6 class can.
+
+=end pod
+
+module JSON::Marshal:ver<v0.0.1>:auth<github:jonathanstowe> {
 
     use JSON::Tiny;
 
